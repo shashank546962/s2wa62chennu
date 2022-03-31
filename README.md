b0095ec43dcffab5058330a95ed1353e99cb1d46
shashank546962 <98129286+shashank546962@users.noreply.github.com>
2022-03-30 22:58:13 -0500

    added computation file

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # s2wa62chennu
 Hi this is shashi
+
 Heroku repo: https://s2wa62chennu.herokuapp.com/
--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('mydata', { title: 'Shashankar Venkata Chennu' });
+  res.render('mydata', { title: 'Anusha Poreddy' });
 });
 
 module.exports = router;
