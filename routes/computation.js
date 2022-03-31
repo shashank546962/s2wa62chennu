0e15789993003e3ff29bd29f41559534b8be2fba
shashank546962 <98129286+shashank546962@users.noreply.github.com>
2022-03-30 20:38:12 -0500

    added computation file

---
+++ routes/computation.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+var storeValue;
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    
+    fake_url = "https://fake.com/path" + req.url
+      const url = new URL(fake_url)
+      const search_params = url.searchParams
+
+    
+      storeValue=Number(search_params.get("x"))
+    // if paramter is not specified  value of Number(search_params.get("x")) is always zero
+    //console.log(Number(search_params.get("x"))+"hello")
+    if(storeValue == 0)
+
+    storeValue=Math.random()
+
+    res.write('Math.cbrt() applied to '+storeValue+" is "+Math.cbrt(storeValue)+"\n");
+    res.end('Math.atanh() applied to '+storeValue+" is "+Math.atanh(storeValue));
+ });
+
+
+module.exports = router;
